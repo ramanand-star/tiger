775b8502f5f8f74c5599ccb792243353f3455648
ramanand-star <72610379+ramanand-star@users.noreply.github.com>
2024-12-24 01:01:43 +0530

    Add files via upload

---
+++ src/main/java/com/tiger/analyst/tiger/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tiger.analyst.tiger;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TigerApplication.class);
+	}
+
+}
