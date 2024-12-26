74a4c82692c3939afbe1cf23a0d2e38b1a2a8aff
ramanand-star <72610379+ramanand-star@users.noreply.github.com>
2024-12-26 20:06:49 +0530

    Add files via upload

--- src/main/java/com/tiger/analyst/tiger/config/WebConfig.java
+++ src/main/java/com/tiger/analyst/tiger/config/WebConfig.java
@@ -8,6 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        //registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://test-pricing-app.s3-website.us-east-2.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
